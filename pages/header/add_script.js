f2f0a5dd829c3349a320890db430ba68ed4fd02f
Alexander Gribkov <gribkovalexander@gmail.com>
2023-12-03 18:54:48 +0300

    Nav edit

--- pages/header/add_script.js
+++ pages/header/add_script.js
@@ -7,12 +7,10 @@ document.body.innerHTML +=
             <nav id="up_nav">
                 <input type="checkbox" id="nav_menu_button"><label for="nav_menu_button"></label>
                     <ul class="nav_list">
+                        <li><a href="../main/"><p>Главная</p></a></li>
                         <li><a href="../products/"><p>Каталог</p></a></li>
-                        <li><a href="#DVS_position"><p>О ДВС</p></a></li>
-                        <li><a href="#wish_position"><p>Желания</p></a></li>
-                        <li><a href="#pictures_position"><p>Галерея</p></a></li>
-                        <li><a href="#order_position"><p>Заказать</p></a></li>
-                        <li><a href="#shop_info_position"><p>О нас</p></a></li>
+                        <li><a href="../cart/"><p>Корзина</p></a></li>
+                        <li><a href="../faq/"><p>FAQ</p></a></li>
                     </ul>
             </nav>
     </header>`
