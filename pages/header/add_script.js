432d3b786c2482fb7bac4e7b277bcdb60b73bf89
Alexander Gribkov <gribkovalexander@gmail.com>
2023-12-04 09:58:05 +0300

    Catalog page add

--- pages/header/add_script.js
+++ pages/header/add_script.js
@@ -8,9 +8,9 @@ document.body.innerHTML +=
                 <input type="checkbox" id="nav_menu_button"><label for="nav_menu_button"></label>
                     <ul class="nav_list">
                         <li><a href="../main/"><p>Главная</p></a></li>
-                        <li><a href="../products/"><p>Каталог</p></a></li>
+                        <li><a href="../catalog/"><p>Каталог</p></a></li>
                         <li><a href="../cart/"><p>Корзина</p></a></li>
-                        <li><a href="../faq/"><p>FAQ</p></a></li>
+                        <li><a href="../products/"><p>FAQ</p></a></li>
                     </ul>
             </nav>
     </header>`
