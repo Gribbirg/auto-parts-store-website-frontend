510cc9162ca08b483bda1c5cf7a58173e42592ae
Alexander Gribkov <gribkovalexander@gmail.com>
2023-12-08 22:41:37 +0300

    Theme select

--- pages/footer/add_script.js
+++ pages/footer/add_script.js
@@ -28,9 +28,11 @@ document.body.innerHTML += `
     </address>
     <div>
         Выберите цветовую тему:
-        <label><input type="radio" name="theme_input" id="theme+theme_input">Как на устройстве</label>
-        <label><input type="radio" name="theme_input" id="theme.light+theme_input">Светлая тема</label>
-        <label><input type="radio" name="theme_input" id="theme.dark+theme_input">Темная тема</label>
+        <div>
+            <div><input type="radio" name="theme_input" id="theme+theme_input" checked><label for="theme+theme_input">Как на устройстве</label></div>
+            <div><input type="radio" name="theme_input" id="theme.light+theme_input"><label for="theme.light+theme_input">Светлая тема</label></div>
+            <div><input type="radio" name="theme_input" id="theme.dark+theme_input"><label for="theme.dark+theme_input">Темная тема</label></div>
+        </div>
     </div>
     <div>
         Курсовая работа
@@ -42,3 +44,22 @@ document.body.innerHTML += `
     </footer>
 `
 
+let theme = localStorage.getItem("theme");
+if (!theme || theme === "") theme = "theme"
+else document.getElementById(`${theme}+theme_input`).checked = true;
+setTheme(theme);
+
+document.querySelectorAll(`input[name="theme_input"]`).forEach(function (input) {
+    input.onchange = function () {
+        if (input.checked) {
+            theme = input.id.split("+")[0];
+            setTheme(theme);
+        }
+    }
+});
+
+
+function setTheme(theme) {
+    document.getElementById("theme_link").href = `/AutoPartsStoreWebsiteFrontend/pages/colors/${theme}.css`;
+    localStorage.setItem("theme", theme);
+}
